aed4c0fc0062fc08410a21df547dc68d8241e627
Caleb <caleb26502@gmail.com>
2022-10-25 00:11:17 +0800

    length

--- SQL/CreateProceduresAndTriggers.sql
+++ SQL/CreateProceduresAndTriggers.sql
@@ -3,7 +3,7 @@
 DELIMITER //
 
 CREATE TRIGGER FESTIVAL_RUNTIME_TRIGGER BEFORE INSERT 
-ON Festival FOR EACH ROW BEGIN 
+ON FESTIVAL FOR EACH ROW BEGIN 
 	IF NEW.runtime < 0 THEN SET NEW.runtime = 0;
 	END IF;
 END; 
@@ -12,17 +12,19 @@ END;
 
 DELIMITER ;
 
--- Create a trigger for year field to make sure it is greater than 1927 and raise an error if it isn't
+-- Create a trigger for year field to make sure it is greater than 1927 and raise an error if it isn't and make it's length 0 if length is null
 
 -- We don't automatically fix this as it is likely a typo or invalid data
 
 DELIMITER //
 
-CREATE TRIGGER YEAR_TRIGGER BEFORE INSERT ON Film FOR 
-EACH ROW BEGIN 
+CREATE TRIGGER FILM_YEAR_TRIGGER BEFORE INSERT ON FILM 
+FOR EACH ROW BEGIN 
 	IF NEW.year < 1927 THEN SIGNAL SQLSTATE '45000'
 	SET
-	    MESSAGE_TEXT = 'Year must be greater than 1928';
+	    MESSAGE_TEXT = 'Year must be greater than 1927';
+	END IF;
+	IF NEW.length IS NULL THEN SET NEW.length = 0;
 	END IF;
 END; 
 
@@ -78,7 +80,7 @@ DELIMITER ;
 DELIMITER //
 
 CREATE TRIGGER FESTIVAL_DELETE_TRIGGER BEFORE DELETE 
-ON Festival FOR EACH ROW BEGIN 
+ON FESTIVAL FOR EACH ROW BEGIN 
 	DELETE FROM Scheduled_Film
 	WHERE
 	    festival_id = OLD.festival_id;
@@ -93,7 +95,7 @@ DELIMITER ;
 DELIMITER //
 
 CREATE TRIGGER VENUE_UPDATE_TRIGGER BEFORE UPDATE ON 
-Venue FOR EACH ROW BEGIN 
+VENUE FOR EACH ROW BEGIN 
 	IF NEW.year <> OLD.year THEN
 	DELETE FROM Scheduled_Film
 	WHERE
